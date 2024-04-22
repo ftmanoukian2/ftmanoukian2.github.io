84314536faed4f55b7f9a21a5d0ab2bf89f3d2c3
ftmanoukian <ftmanoukian@gmail.com>
2024-04-22 19:48:58 -0300

    agrego comentarios ejemplo ej5

--- extras/ordenar_lista.py
+++ extras/ordenar_lista.py
@@ -1,6 +1,13 @@
 """
 Ejemplo realizado en clase el 18/4/2024
 Ejercicio 5 de listas
+
+Procedimiento:
+    1 - Definir una lista vacía para almacenar los elementos ordenados ('lista_ord')
+    2 - Encontrar el elemento más chico de la lista desordenada ('lista_desord')
+    3 - Agregar el elemento encontrado al final de la nueva lista lista ('lista_ord')
+    4 - Remover el elemento encontrado de la lista desordenada ('lista_desord')
+    5 - Repetir desde el paso 2 hasta que haber removido todos los elementos de la lista desordenada
 """
 
 # Creo una lista desordenada que voy a pasar a la función
@@ -14,11 +21,12 @@ def ordenar_lista(lista_desord):
     # Repetición hasta paso 5
     while len(lista_desord) > 0:
         # 2 - Encontrar el número más chico
-        mas_chico = lista_desord[0]
-        for elemento in lista_desord[1:]:
+        mas_chico = lista_desord[0]             # suponemos que el elemento más chico es el primero (esto es sólo para iniciar el bucle - probablemente lo que asumimos no sea así).
+        for elemento in lista_desord[1:]:       # comparamos el elemento más chico con todos los elementos de la lista desordenada. empezamos desde el segundo, para no comparar el primero consigo mismo.
             if mas_chico > elemento:
-                mas_chico = elemento
-
+                mas_chico = elemento            # si el elemento con el que estamos comparando es más chico que el que pensábamos, lo cambiamos. mas_chico pasa a contener el menor encontrado hasta el momento.
+                                                # al finalizar el bucle, en la variable mas_chico no puede haber otro número que no sea el más chico de la lista desordenada.
+                                                
         # 3 - Agregar el número encontrado al final de lista_ord
         lista_ord.append(mas_chico)
 
